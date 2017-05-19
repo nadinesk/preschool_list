aeb0057fa790a22690c8a7bae32bd606d02c0564
nadinesk <nadine.khattak@gmail.com>
2017-05-19 15:09:02 -0400

    update user_controller.rb with sign up

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,29 @@
 class FiguresController < ApplicationController
 
-  
+  get '/signup' do
+    if !session[:user_id]
+      erb :'users/signup'
+    else
+      redirect to '/preschools'
+    end
+  end
+
+  get '/preschools' do
+    if session[:user_id]
+      @preschools = Preschool.all
+      erb :'preschool/preschools'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get "/login" do
+   if session[:user_id]
+     redirect "/preschools"
+   else
+     erb :'users/login'
+   end
+ end
+
 
 end
