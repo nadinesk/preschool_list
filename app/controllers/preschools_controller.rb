6b61e478abe7042b9e127b93a635cdb1f492acac
nadinesk <nadine.khattak@gmail.com>
2017-05-20 18:42:53 -0400

    added redirect_if_not_logged_in helper method and refactored prechool_applciation_controler with that function - yet to test

--- app/controllers/preschools_controller.rb
+++ app/controllers/preschools_controller.rb
@@ -1,90 +1,75 @@
 class PreschoolsController < ApplicationController
 
-	get '/preschools' do    
-    	
-	     if logged_in?
-
-	      #@user = User.find(session[:user_id])
-	      
+	get '/preschools' do        	
+	     redirect_if_not_logged_in
 	      @preschools = Preschool.all 
 	      erb :'preschools/preschools'
-	    else
-	      redirect to '/'
 	    end
 	 end
     	
 
   	get '/preschools/new' do
-    	 if logged_in?
-      		erb :'preschools/create'
-    	else
-      		redirect "/login"
-    	end
+    	redirect_if_not_logged_in
+      @error_message = params[:error]
+      erb :'preschools/create'    	
     end
 
-    post '/preschools/new' do
-    	
-    	if params[:name] == "" || params[:address] == "" || params[:cost] == "" || params[:summary] == ""
-      		redirect "preschools/new"
-    	else
-      		@preschool = Preschool.create(:name => params[:name], :address => params[:address],:cost => params[:cost],:summary => params[:summary])
-      		
-      		@preschool.user_id = current_user.id
-      		@preschool.save
-      		redirect to("/preschools/#{@preschool.id}")
-    	end
-  	end
+    get '/preschools/:id/edit' do
+         redirect_if_not_logged_in
+        @error_message = params[:error]
+        @preschool = Preschool.find_by_id(params[:id])
+        @user = User.find(current_user.id)
+        erb :'preschools/edit'
+    end
+
+     post '/preschools/new' do
+      redirect_if_not_logged_in
+      unless Preschool.valid_params?(params)
+        redirect "/preschools/new?error=invalid preschool entry"
+      
+          @preschool = Preschool.create(:name => params[:name], :address => params[:address],:cost => params[:cost],:summary => params[:summary])
+          
+          @preschool.user_id = current_user.id
+          @preschool.save
+          redirect to("/preschools/#{@preschool.id}")
+      end
+    end
+
+    patch '/preschools/:id' do            
+        redirect_if_not_logged_in
+        @preschool =  Preschool.find_by_id(params[:id])
+        unless Preschool.valid_params?(params)
+          redirect "/preschools/#{@preschool.id}/edit?error=invalid preschool entry"
+        end
+       
+        @preschool.update(:name => params[:name], :address => params[:address],:cost => params[:cost],:summary => params[:summary])
+        @preschool.save
+          
+        redirect to("/preschools/#{@preschool.id}")
+        end
+    end
+
+    get '/preschools/:id' do
+      redirect_if_not_logged_in
+      @preschool = Preschool.find(params[:id])
+      @user = User.find(@preschool.user_id)
+      erb :'preschools/show'
+    end
 
 
   	get '/preschools/:id' do
-    	if logged_in?
+    	redirect_if_not_logged_in
       		@preschool = Preschool.find(params[:id])
       		@user = User.find(@preschool.user_id)
-      		erb :'preschools/show'
-    	else
-      		redirect "/login"
-    	end
-  	end
-
-  	get '/preschools/:id/edit' do
-	     if logged_in?
-      		@preschool = Preschool.find_by_id(params[:id])
-      		@user = User.find(current_user.id)
-      		if @preschool.user_id == current_user.id
-       			erb :'preschools/edit'
-      		else
-        		redirect to '/preschools'
-      		end
-    	  else
-      		redirect to '/login'
-    	  end
-
-  	end
-
-  	patch '/preschools/:id' do        		
-      	@preschool =  Preschool.find_by_id(params[:id])
-      	if params[:name] == "" || params[:address] == "" || params[:cost] == "" || params[:summary] == ""     
-        	redirect to("/preschools/#{@preschool.id}/edit")
-      	else
-        	@preschool.update(:name => params[:name], :address => params[:address],:cost => params[:cost],:summary => params[:summary])
-	        @preschool.save
-	        
-    	    redirect to("/preschools/#{@preschool.id}")
-      	end
+      		erb :'preschools/show'    	
   	end
 
   	delete '/preschools/:id/delete' do #delete action     
-     	if logged_in?
+     	redirect_if_not_logged_in
       		@preschool = Preschool.find_by_id(params[:id])
-      		if @preschool.user_id == current_user.id
-        		@preschool.delete
-       		 	redirect to '/preschools'
-      		else
-        		redirect to '/preschools'
-      		end
-    	else
-      		redirect to '/login'
-    	end
+      		
+        	redirect to '/preschools'
+      		
   	end
 
  
--- app/models/preschool.rb
+++ app/models/preschool.rb
@@ -1,4 +1,9 @@
 class Preschool <ActiveRecord::Base
 	belongs_to :user
 
+	def self.valid_params?(params)
+	    return !params[:name].empty? && !params[:address].empty? && !params[:cost].empty? && !params[:summary].empty?
+  	end
+
+
 end
